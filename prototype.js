0337973105537d04efe7d0978185b821d64388aa
Andrew Watterson <awatterson@google.com>
2019-01-16 17:27:34 -0800

    Initial commit.

---
+++ prototype.js
@@ -0,0 +1,95 @@
+var defaultTimeout = 3000;
+var defaultPrototype = "dots";
+var defaultSelector = 1;
+
+var prototypes = {
+  dots: {
+    topFrameId: "twitterTopFrame",
+    contentId: "twitterContent",
+    loaderId: "dots"
+  },
+  spinner: {
+    topFrameId: "twitterTopFrame",
+    contentId: "twitterContent",
+    loaderId: "spinner"
+  },
+  inertialSpinner: {
+    topFrameId: "twitterTopFrame",
+    contentId: "twitterContent",
+    loaderId: "inertialSpinner"
+  },
+  inertialStaged: {
+    topFrameId: "twitterTopFrame",
+    contentId: "twitterContent",
+    loaderId: "inertialStaged"
+  },
+  twitterStaged: {
+    topFrameId: "twitterTopFrame",
+    contentId: "twitterContent",
+    loaderId: "twitterStaged"
+  },
+  adStaged: {
+    topFrameId: "twitterTopFrame",
+    contentId: "adContent",
+    loaderId: "adStaged"
+  },
+  twitterStatic: {
+    topFrameId: "twitterTopFrame",
+    contentId: "twitterContent",
+    loaderId: "twitterStatic"
+  },
+  twitterPulse: {
+    topFrameId: "twitterTopFrame",
+    contentId: "twitterContent",
+    loaderId: "twitterPulse"
+  },
+  twitterPulseStaged: {
+    topFrameId: "twitterTopFrame",
+    contentId: "twitterContent",
+    loaderId: "twitterPulseStaged"
+  }
+}
+
+var urlParams = new URLSearchParams(window.location.search);
+var timeout = urlParams.get('timeout') || defaultTimeout;
+var prototypeId = urlParams.get('p') || defaultPrototype;
+var showSelector = urlParams.get('s') || defaultSelector;
+
+function loaderFadeOut(prototype) {
+  var loader = document.getElementById(prototype.loaderId);
+  loader.addEventListener('transitionend', function(evt) {
+    evt.target.style.display = "none";
+  });
+  loader.className += " fadeOut";
+}
+
+function contentFadeIn(prototype) {
+  var replacement = document.getElementById(prototype.contentId);
+  replacement.className += " fadeIn";
+}
+
+function setUpPrototype(prototype) {
+  hideAllButId('top-frame', prototype.topFrameId);
+  hideAllButId('placeholder', prototype.loaderId);
+  hideAllButId('content', prototype.contentId);
+
+  removeClassFromAll('fadeOut');
+  removeClassFromAll('fadeIn');
+
+  setTimeout(() => {
+    loaderFadeOut(prototype);
+    contentFadeIn(prototype);
+  }, timeout);
+}
+
+window.addEventListener('load', function() {
+
+  if(!showSelector) {
+    var selector = document.getElementsByClassName('selector-wrapper')[0];
+    selector.style.display = "none";
+  } else {
+    setUpSelector(prototypes, prototypeId);
+  }
+
+  setUpPrototype(prototypes[prototypeId]);
+});
---
+++ selector.js
@@ -0,0 +1,42 @@
+function reactToSelector(selector) {
+  prototypeId = selector.value;
+  setUpPrototype(prototypes[prototypeId]);
+}
+
+function hideAllButId(className, id) {
+  var elements = document.getElementsByClassName(className);
+  for(e in elements) {
+    if(elements[e].id && elements[e].id !== id) {
+      elements[e].style.display = 'none';
+    } else if(elements[e].id) {
+      elements[e].style.display = 'block';
+    }
+  }
+}
+
+function removeClassFromAll(className) {
+  var elements = document.getElementsByClassName(className);
+  for(e in elements) {
+    if(elements.hasOwnProperty(e)) {
+      var classes = elements[e].className.split(" ");
+      classes.indexOf(className) > 0 && classes.splice(classes.indexOf(className),1);
+      elements[e].className = classes.join(" ");
+    }
+  }
+}
+
+function setUpSelector(options, selected) {
+  var selector = document.getElementById('spinner-selector');
+  selector.addEventListener('change', function(evt) { reactToSelector(evt.target); });
+  for(s in options) {
+    if(options.hasOwnProperty(s)) {
+      var option = document.createElement('option');
+      option.appendChild(document.createTextNode(s));
+      option.setAttribute('value', s);
+      (s === selected) && option.setAttribute('selected', true);
+      selector.appendChild(option);
+    }
+  }
+
+  return selector;
+}
